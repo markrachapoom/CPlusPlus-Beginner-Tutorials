c368cf94e1a21fb74400f1aa3969d073de540d4c
markkypanc <73297926+markkypanc@users.noreply.github.com>
2021-01-15 22:43:21 +0700

    Add files via upload

--- Episode_8_Conditions.cpp
+++ Episode_8_Conditions.cpp
@@ -1,8 +1,13 @@
 // Episode 8: Conditions
+
 /*
  It's pretty straight forward in this episode
  
- Pseudocode
+ What you'll know from this: if...else statement and Condition Operator ("?")
+ 
+ -----------------------------------------------------------------------------
+ 
+ IF...ELSE STATEMENT
  
  if something happend
     Do this
@@ -11,6 +16,16 @@
  
  it's easy right??? seems like it's a common sense rather than a technical coding
  
+ -----------------------------------------------------------------------------
+ 
+ TERNARY OPERATOR OR '?'
+ 
+ condition ? do this : do that
+ 
+ How to read...
+ Is condition true? if yes, do this; if not, do that
+ 
+ -----------------------------------------------------------------------------
  */
 
 #include <iostream>
@@ -22,11 +37,16 @@ int main(int argc, char* argv[]) {
     
     int grade = 80;
     
+    // Control Flows
     if (grade >= 60) {          // If score is greater than or equals 60
-        cout << "Passed";       // Do this!
+        cout << "Passed\n";       // Do this!
     } else {                    // If not...
-        cout << "Failed...";    // Do this instead...
+        cout << "Failed...\n";    // Do this instead...
     }
+    
+    // Ternary Operator
+    grade >= 60 ? cout << "You are passed!\n\n" : cout << "You are failed..."; // Method 1
+    cout << (grade >= 60 ? "You are passed!\n\n" : "You are failed...\n"); // Method 2
     
     return 0;
 }
